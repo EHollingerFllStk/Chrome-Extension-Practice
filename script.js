e5e21d7b7d61fc14e5c7d23621426b947adfcba1
Elizabeth Hollinger <111613075+EHollingerFllStk@users.noreply.github.com>
2023-03-03 20:54:38 +0000

    add DOM element to change ul

--- script.js
+++ script.js
@@ -20,9 +20,10 @@
 //myLeads --> should be assigned to an empty array
 //inputEl -> should be assigned to the text input 
 
-let myLeads = [];
-const inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
+// let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+// const inputEl = document.getElementById("input-el")
+// let inputBtn = document.getElementById("input-btn")
+
 
 // push the value "www.awesomelead.com" to myArray when the input button is clicked
 // inputBtn.addEventListener("click", function() {
@@ -31,15 +32,45 @@ let inputBtn = document.getElementById("input-btn")
 //   //console.log("Button clicked")
 // })
 
+// inputBtn.addEventListener("click", function() {
+//   // Push the value from the inputEl into the myLeads array
+//   // instead of the hard-coded "www.awesomelead.com" 
+//   // Google -> "get value from input field javascript"
+//   myLeads.push(inputEl.value)
+//   console.log(myLeads)
+//   //console.log("Button clicked")
+// })
 
+// log out the items in the myLeads array using a for loop
+
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+let inputBtn = document.getElementById("input-btn");
+// 2. Grab the unordered list and store it in a const variable caled ulEl
+const ulEl = document.getElementById("ul-el");
+//console.log(ulEl)
+
+// inputBtn.addEventListener("click", function() {
+//   myLeads.push(inputEl.value)
+//   for (i = 0; i < myLeads.length; i++) {
+//   ulEl.textContent = myLeads[i]
+//   console.log(myLeads)
+//   }
+// })
+// // render the leads in the unordered list using the ulEl.textContent
+// for (i = 0; i < myLeads.length; i++) {
+//   ulEl.textContent = myLeads[i]
+//   console.log(myLeads[i])
+// }
+
+//Per's solution:
 
 inputBtn.addEventListener("click", function() {
-  // Push the value from the inputEl into the myLeads array
-  // instead of the hard-coded "www.awesomelead.com" 
-  // Google -> "get value from input field javascript"
   myLeads.push(inputEl.value)
   console.log(myLeads)
-  //console.log("Button clicked")
 })
 
+for (i = 0; i < myLeads.length; i++) {
+  ulEl.textContent += myLeads[i] + " "
+}
 
