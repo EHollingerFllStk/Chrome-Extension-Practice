8860a03f46babcd794af15f9a225242911127963
Elizabeth Hollinger <111613075+EHollingerFllStk@users.noreply.github.com>
2023-03-03 19:26:30 +0000

    add event listener to box

--- script.js
+++ script.js
@@ -3,8 +3,16 @@
 // }
 // Rather than using the above developers use the above for cleaner seperation of operations
 
-let inputBtn = document.getElementById("input-btn")
-inputBtn.addEventListener("click", function() {
-  console.log("Button clicked from addEvent listener")
+// let inputBtn = document.getElementById("input-btn")
+// inputBtn.addEventListener("click", function() {
+//   console.log("Button clicked from addEvent listener")
+// })
+
+//Grab the box from the DOM and store it in a variable
+let box = document.getElementById("box")
+//add a click event listener to the box
+box.addEventListener("click", function() {
+  //Log out "I want to open the box!" when it's clicked
+  console.log("I want to open the box!")
 })
 
