37d485a2bce1a1f728af30cad140fd5ed4b2b17b
Elizabeth Hollinger <111613075+EHollingerFllStk@users.noreply.github.com>
2023-03-03 20:13:49 +0000

    functionality to push input to myLeads array

--- script.js
+++ script.js
@@ -5,14 +5,41 @@
 
 // let inputBtn = document.getElementById("input-btn")
 // inputBtn.addEventListener("click", function() {
-//   console.log("Button clicked from addEvent listener")
+//   console.log("Button clicked")
 // })
 
-//Grab the box from the DOM and store it in a variable
-let box = document.getElementById("box")
-//add a click event listener to the box
-box.addEventListener("click", function() {
-  //Log out "I want to open the box!" when it's clicked
-  console.log("I want to open the box!")
+// //Grab the box from the DOM and store it in a variable
+// let box = document.getElementById("box")
+// //add a click event listener to the box
+// box.addEventListener("click", function() {
+//   //Log out "I want to open the box!" when it's clicked
+//   console.log("I want to open the box!")
+// })
+
+//Create two variables:
+//myLeads --> should be assigned to an empty array
+//inputEl -> should be assigned to the text input 
+
+let myLeads = [];
+const inputEl = document.getElementById("input-el")
+let inputBtn = document.getElementById("input-btn")
+
+// push the value "www.awesomelead.com" to myArray when the input button is clicked
+// inputBtn.addEventListener("click", function() {
+//   myLeads.push("www.awesomelead.com")
+//   console.log(myLeads)
+//   //console.log("Button clicked")
+// })
+
+
+
+inputBtn.addEventListener("click", function() {
+  // Push the value from the inputEl into the myLeads array
+  // instead of the hard-coded "www.awesomelead.com" 
+  // Google -> "get value from input field javascript"
+  myLeads.push(inputEl.value)
+  console.log(myLeads)
+  //console.log("Button clicked")
 })
+
 
